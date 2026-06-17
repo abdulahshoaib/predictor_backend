1f5e17dd8c5411a166aefef752a8c13c9a8addac
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2026-06-18 00:13:01 +0500

    feat: allow cors

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use axum::{Router, routing::get};
 use handlers::{health::health_check, matches::get_matches};
 use state::AppState;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
     let state = AppState::new();
+
+    let cors = CorsLayer::new().allow_origin(Any);
 
     let app = Router::new()
         .route("/matches", get(get_matches))
